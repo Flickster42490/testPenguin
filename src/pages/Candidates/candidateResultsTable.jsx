024849078f71f6c0318a98f16fd82e62940be2ac
Max Li <>
2018-06-12 17:39:04 -0500

    update logic for candidate dashboard table rows

--- src/pages/Candidates/candidateResultsTable.jsx
+++ src/pages/Candidates/candidateResultsTable.jsx
@@ -283,9 +283,11 @@ export default class Dashboard extends Component {
             }
           ]}
           defaultPageSize={
-            this.state.candidateList.length < 10
-              ? this.state.candidateList.length
-              : 10
+            this.state.candidateList.length <= 5
+              ? 5
+              : this.state.candidateList.length < 10
+                ? this.state.candidateList.length
+                : 10
           }
           pageSizeOptions={[5, 10]}
           className="-striped -highlight"
